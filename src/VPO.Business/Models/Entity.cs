4899f26f342895b303c7eb2afb01fe486890ed64
vpoliveira <vpoliveira@cpqd.com.br>
2020-11-05 21:49:47 -0300

    feat: setup da aplicação

---
+++ src/VPO.Business/Models/Entity.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace VPO.Business.Models
+{
+    public abstract class Entity
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+}
